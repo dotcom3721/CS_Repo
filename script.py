57ad9941e9b3698a87805f2145ad6ccc95ee685d
dotcom3721 <dotcom.lee@yahoo.com>
2021-08-12 14:55:51 -0400

    c1

--- script.py
+++ script.py
@@ -4,14 +4,14 @@ Created on Thu Aug 12 11:11:10 2021
 
 @author: ali1
 """
-import sys
+import requests
 
-print(sys.version)
-print(sys.executable)
+# print(sys.version)
+# print(sys.executable)
 
 def greet(who_to_greet):
     greeting = 'Hello, {}'.format(who_to_greet)
     return greeting
 
-print(greet('world'))
-print(greet('Corey'))
+r = requests.get("https://coreyms.com")
+print(r.status_code)
